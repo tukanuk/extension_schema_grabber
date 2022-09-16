43459e1e2bef4c3303d5f7fd898ce482e8d13ef2
Ben Davidson <ben.davidson@dynatrace.com>
2022-09-16 15:30:58 -0400

    Clean up repo

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from email.encoders import encode_quopri
 import yaml
 
-with open("config/config.yaml", "r", encoding="UTF-8") as file:
+with open("config.yaml", "r", encoding="UTF-8") as file:
     config_file = yaml.safe_load(file)
 
 token = config_file['token']
